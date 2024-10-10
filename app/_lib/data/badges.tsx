3fd26063290886b32b92921977dd72c848d560ad
Alejandro Zamora <184447608+alejandrozamora-az@users.noreply.github.com>
2024-10-09 18:56:12 -0700

    Version 5.4.100 Update badges.tsx

--- app/_lib/data/badges.tsx
+++ app/_lib/data/badges.tsx
@@ -3,26 +3,26 @@ import { Badge } from '@/common/definitions';
 export const badges: Badge[] = [
     {
         name: "Unity Junior Programmer",
-        imageUrl: "https://res.cloudinary.com/dlyfwab7x/image/upload/v1704136401/badges/sml9oqkpkbg5zuxtsbs9.webp",
-        href: "https://www.credly.com/badges/f3aec58a-2568-4f4b-9ee3-e98da34986a0/public_url",
+        imageUrl: "https://i.postimg.cc/Vk4xQGDt/junior-programmer.webp",
+        href: "https://www.credly.com/badges/85215e9f-73ff-4ded-9218-6cc4fa0db4da/public_url",
         alt: "A purple and black badge with the words junior programming program and an image of a square object on top of it."
     },
     {
         name: "Unity Creative Core",
-        imageUrl: "https://res.cloudinary.com/dlyfwab7x/image/upload/v1704136402/badges/rcqtsf8amj6ri4fz6q8x.webp",
-        href: "https://www.credly.com/badges/7df6efb7-7279-4130-8bca-48622f240f85/public_url",
+        imageUrl: "https://i.postimg.cc/3x3sq5C7/creative-core.webp",
+        href: "https://www.credly.com/badges/36f6b830-9b71-4148-bf2e-7fd864104a6f/public_url",
         alt: "A black and blue hexagonal badge with the words creative core in the center of the image and a white hexagonal."
     },
     {
         name: "Unity Mobile AR Development",
-        imageUrl: "https://res.cloudinary.com/dlyfwab7x/image/upload/v1704136402/badges/ixgvcw3yytqt9c9oada2.webp",
-        href: "https://www.credly.com/badges/ab1141eb-2642-4c4d-a6e4-8619dacb2f74/public_url",
+        imageUrl: "https://i.postimg.cc/R0D2q3rV/mobile-ar-development.webp",
+        href: "https://www.credly.com/badges/6672bd54-77fa-4a73-a1ad-470a6f55061f/public_url",
         alt: "A black and pink logo with the words mobile ar development on the bottom of it, and a pink and black hexagon."
     },
     {
         name: "AWS Cloud Quest: Cloud Practitioner",
-        imageUrl: "https://res.cloudinary.com/dlyfwab7x/image/upload/v1704136402/badges/mtr9jfqb1skrcabmx2va.webp",
-        href: "https://www.credly.com/badges/64706d66-220c-4461-b372-74dbaf3cdafa/public_url",
+        imageUrl: "https://i.postimg.cc/q7R9ks55/cloud-practitioner.webp",
+        href: "https://www.credly.com/badges/a8aae375-401e-4570-bfc3-88d253cbf397/public_url",
         alt: "The aws cloud quest logo with the words cloud practioner above it and a laptop in the background."
     }
 ];
